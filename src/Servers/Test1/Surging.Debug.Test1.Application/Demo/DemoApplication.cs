ea04884410ff00ce12ba9b950f1468c0fc59bdde
liuhll <1029765111@qq.com>
2019-01-27 20:03:27 +0800

    新增用于调试Surging源码的服务组件

---
+++ src/Servers/Test1/Surging.Debug.Test1.Application/Demo/DemoApplication.cs
@@ -0,0 +1,27 @@
+﻿using Surging.Core.ProxyGenerator;
+using Surging.Debug.Test1.IApplication.Demo;
+using Surging.Debug.Test1.IApplication.Demo.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Surging.Debug.Test1.Application.Demo
+{
+    public class DemoApplication : ProxyServiceBase, IDemoApplication
+    {
+        public async Task<string> GetUserName(QueryUserInput input)
+        {
+            var id = await GetService<IServiceProxyProvider>().Invoke<string>(new Dictionary<string, object>() {
+                { "id", Guid.NewGuid().ToString()},
+
+            }, "v1/api/debug/demo/getuserid");
+            return input.UserId + Guid.NewGuid() + id;
+        }
+
+        public async Task<string> GetUserId(string id)
+        {
+            return id;
+        }
+    }
+}
---
+++ src/Servers/Test1/Surging.Debug.Test1.IApplication/Demo/IDemoApplication.cs
@@ -0,0 +1,18 @@
+﻿using Surging.Core.CPlatform.Ioc;
+using Surging.Core.CPlatform.Runtime.Server.Implementation.ServiceDiscovery.Attributes;
+using Surging.Debug.Test1.IApplication.Demo.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Surging.Debug.Test1.IApplication.Demo
+{
+    [ServiceBundle("v1/api/debug/demo/{service}")]
+    public interface IDemoApplication : IServiceKey
+    {
+        Task<string> GetUserName(QueryUserInput input);
+
+        Task<string> GetUserId(string id);
+    }
+}
