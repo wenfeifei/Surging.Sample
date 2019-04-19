ba9a5a99a3dce5b08a76aa2e2d2fd603d0096277
liuhongliang <1029765111@qq.com>
2019-04-20 01:16:23 +0800

    更新FullAuditedEntity审计实体对象

--- src/Core/Surging.Core.Domain/Entities/Auditing/FullAuditedEntity.cs
+++ src/Core/Surging.Core.Domain/Entities/Auditing/FullAuditedEntity.cs
@@ -18,17 +18,17 @@ namespace Surging.Core.Domain.Entities.Auditing
         public virtual DateTime? DeletionTime { get; set; }
     }
 
-    //[Serializable]
-    //public abstract class FullAuditedEntity<TPrimaryKey, TUser> : AuditedEntity<TPrimaryKey, TUser>, IFullAudited<TUser>
-    //    where TUser : IEntity<string>
-    //{
-    //    public virtual int IsDeleted { get; set; }
+    [Serializable]
+    public abstract class FullAuditedEntity<TPrimaryKey, TUser> : AuditedEntity<TPrimaryKey, TUser>, IFullAudited<TUser>
+        where TUser : IEntity<long>
+    {
+        public virtual int IsDeleted { get; set; }
 
-    //    [ForeignKey("DeleterUserId")]
-    //    public virtual TUser DeleterUser { get; set; }
+        [ForeignKey("DeleterUserId")]
+        public virtual TUser DeleterUser { get; set; }
 
-    //    public virtual long? DeleterUserId { get; set; }
+        public virtual long? DeleterUserId { get; set; }
 
-    //    public virtual DateTime? DeletionTime { get; set; }
-    //}
+        public virtual DateTime? DeletionTime { get; set; }
+    }
 }
