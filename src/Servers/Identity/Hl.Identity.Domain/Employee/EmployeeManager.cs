d601a281ff16960b04557a012d8f7249e121e648
liuhongliang <1029765111@qq.com>
2019-05-01 20:05:06 +0800

    将微服务组件BasicData增加到HlWithIdentity解决方案中

--- src/Servers/Identity/Hl.Identity.Domain/Employee/EmployeeManager.cs
+++ src/Servers/Identity/Hl.Identity.Domain/Employee/EmployeeManager.cs
@@ -13,7 +13,6 @@ namespace Hl.Identity.Domain.Employee
         public async Task CreateEmployee(EmployeeAggregate employee)
         {
            
-
             await UnitOfWorkAsync(async (conn, tran) =>
             {
                 var employeeId = await GetService<IDapperRepository<EmployeeAggregate, long>>().InsertAndGetIdAsync(employee,conn,tran);
