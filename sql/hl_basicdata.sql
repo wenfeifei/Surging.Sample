7ebe1ca625f4c833ba4571da4018624ceade7d24
liuhongliang <1029765111@qq.com>
2019-05-13 20:52:15 +0800

    更新sql脚本

--- sql/hl_basicdata.sql
+++ sql/hl_basicdata.sql
@@ -16,13 +16,12 @@ create table bd_dictionary
 (
    Id                   bigint not null auto_increment comment '主键',
    Code                 varchar(50) not null comment '唯一编码',
-   Name                 varchar(50) not null comment '字典名称',
-   Value                varchar(50) not null comment '字典值',
+   Name                 varchar(50) not null comment '名称',
    ParentId             bigint not null comment '父级Id',
    Seq                  int not null comment '序号',
-   TypeName             varchar(50) comment '分类名称',
+   SysPreSet          int not null comment '0. 否 1.是',
+   TypeName             varchar(50) not null comment '分类名称',
    HasChild             int not null comment '0.没有 1.有',
-   IsSysPreSet          int not null comment '0. 否 1.是',
    CreateBy             bigint comment '创建人',
    CreateTime           datetime comment '创建日期',
    UpdateBy             bigint comment '修改人',
@@ -35,12 +34,10 @@ create table bd_dictionary
 
 alter table bd_dictionary comment '字典表';
 
-INSERT INTO `hl_basicdata`.`bd_dictionary`(`Id`, `Code`, `Value`, `ParentId`, `Seq`, `TypeName`, `HasChild`, `IsSysPreSet`, `CreateBy`, `CreateTime`, `UpdateBy`, `UpdateTime`, `IsDeleted`, `DeleteBy`, `DeleteTime`) VALUES (1, 'systemconf', '系统设置', 0, 1, '系统设置', 1, 1, NULL, '2019-05-01 22:05:47', NULL, NULL, 0, NULL, NULL);
-INSERT INTO `hl_basicdata`.`bd_dictionary`(`Id`, `Code`, `Value`, `ParentId`, `Seq`, `TypeName`, `HasChild`, `IsSysPreSet`, `CreateBy`, `CreateTime`, `UpdateBy`, `UpdateTime`, `IsDeleted`, `DeleteBy`, `DeleteTime`) VALUES (2, 'systemconf_userpdwmode', '密码生成模式', 1, 1, '系统设置', 1, 1, NULL, '2019-05-01 22:07:37', NULL, NULL, 0, NULL, NULL);
-INSERT INTO `hl_basicdata`.`bd_dictionary`(`Id`, `Code`, `Value`, `ParentId`, `Seq`, `TypeName`, `HasChild`, `IsSysPreSet`, `CreateBy`, `CreateTime`, `UpdateBy`, `UpdateTime`, `IsDeleted`, `DeleteBy`, `DeleteTime`) VALUES (3, 'systemconf_userpdwmode_random', '随机密码生成模式', 2, 1, '系统设置', 0, 1, NULL, '2019-05-01 22:10:41', NULL, NULL, 0, NULL, NULL);
-INSERT INTO `hl_basicdata`.`bd_dictionary`(`Id`, `Code`, `Value`, `ParentId`, `Seq`, `TypeName`, `HasChild`, `IsSysPreSet`, `CreateBy`, `CreateTime`, `UpdateBy`, `UpdateTime`, `IsDeleted`, `DeleteBy`, `DeleteTime`) VALUES (4, 'systemconf_userpdwmode_fixed', '固定密码生成模式', 2, 2, '系统设置', 0, 1, NULL, '2019-05-01 22:11:37', NULL, NULL, 0, NULL, NULL);
-INSERT INTO `hl_basicdata`.`bd_dictionary`(`Id`, `Code`, `Value`, `ParentId`, `Seq`, `TypeName`, `HasChild`, `IsSysPreSet`, `CreateBy`, `CreateTime`, `UpdateBy`, `UpdateTime`, `IsDeleted`, `DeleteBy`, `DeleteTime`) VALUES (5, 'systemconf_userpwdfixed_val', '123qwe', 2, 1, '系统设置', 0, 1, NULL, '2019-05-01 22:16:06', NULL, NULL, 0, NULL, NULL);
-
+INSERT INTO `hl_basicdata`.`bd_dictionary`(`Id`, `Code`, `Name`, `ParentId`, `Seq`, `SysPreSet`, `TypeName`, `HasChild`, `CreateBy`, `CreateTime`, `UpdateBy`, `UpdateTime`, `IsDeleted`, `DeleteBy`, `DeleteTime`) VALUES (1, 'systemconf', '系统设置', 0, 1, 1, '系统设置', 1, 0, '2019-05-13 21:25:33', NULL, NULL, 0, NULL, NULL);
+INSERT INTO `hl_basicdata`.`bd_dictionary`(`Id`, `Code`, `Name`, `ParentId`, `Seq`, `SysPreSet`, `TypeName`, `HasChild`, `CreateBy`, `CreateTime`, `UpdateBy`, `UpdateTime`, `IsDeleted`, `DeleteBy`, `DeleteTime`) VALUES (2, 'systemconf_pwd_mode', '密码生成模式', 1, 2, 1, '密码生成模式', 1, 0, '2019-05-13 21:26:04', NULL, NULL, 0, NULL, NULL);
+INSERT INTO `hl_basicdata`.`bd_dictionary`(`Id`, `Code`, `Name`, `ParentId`, `Seq`, `SysPreSet`, `TypeName`, `HasChild`, `CreateBy`, `CreateTime`, `UpdateBy`, `UpdateTime`, `IsDeleted`, `DeleteBy`, `DeleteTime`) VALUES (3, 'systemconf_userpwdmode_random', '随机密码生成模式', 2, 1, 1, '随机密码生成模式', 1, 0, '2019-05-13 21:26:43', NULL, NULL, 0, NULL, NULL);
+INSERT INTO `hl_basicdata`.`bd_dictionary`(`Id`, `Code`, `Name`, `ParentId`, `Seq`, `SysPreSet`, `TypeName`, `HasChild`, `CreateBy`, `CreateTime`, `UpdateBy`, `UpdateTime`, `IsDeleted`, `DeleteBy`, `DeleteTime`) VALUES (4, 'systemconf_userpwdmode_fixed', '固定密码生成模式', 2, 2, 1, '固定密码生成模式', 1, 0, '2019-05-13 21:27:15', NULL, NULL, 0, NULL, NULL);
 
 /*==============================================================*/
 /* Table: bd_systemconf                                         */
@@ -52,7 +49,7 @@ create table bd_systemconf
    ConfigValue          varchar(50) not null comment '配置值',
    Memo                 varchar(100) comment '备注',
    Seq                  int not null comment '序号',
-   IsSysPreSet          int not null comment '0. 否 1.是',
+   SysPreSet          int not null comment '0. 否 1.是',
    CreateBy             bigint comment '创建人',
    CreateTime           datetime comment '创建日期',
    UpdateBy             bigint comment '修改人',
@@ -65,4 +62,6 @@ create table bd_systemconf
 
 alter table bd_systemconf comment 'bd_systemconf';
 
+INSERT INTO `hl_basicdata`.`bd_systemconf`(`Id`, `ConfigName`, `ConfigValue`, `Memo`, `Seq`, `SysPreSet`, `CreateBy`, `CreateTime`, `UpdateBy`, `UpdateTime`, `IsDeleted`, `DeleteBy`, `DeleteTime`) VALUES (1, 'systemconf_pwd_mode', '1', '密码生成模式', 1, 1, 0, '2019-05-13 21:29:11', NULL, NULL, 0, NULL, NULL);
+INSERT INTO `hl_basicdata`.`bd_systemconf`(`Id`, `ConfigName`, `ConfigValue`, `Memo`, `Seq`, `SysPreSet`, `CreateBy`, `CreateTime`, `UpdateBy`, `UpdateTime`, `IsDeleted`, `DeleteBy`, `DeleteTime`) VALUES (2, 'sys_config_pwd_fixed', '123qwe', '固定密码', 2, 1, 0, '2019-05-13 21:30:29', NULL, NULL, 0, NULL, NULL);
 
