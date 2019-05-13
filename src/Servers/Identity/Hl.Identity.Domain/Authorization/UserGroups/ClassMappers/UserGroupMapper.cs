5ac8617ffcf1b54335da3b554fa51970dd7d674e
liuhongliang <1029765111@qq.com>
2019-05-13 23:11:30 +0800

    新增用户组实体和映射类

---
+++ src/Servers/Identity/Hl.Identity.Domain/Authorization/UserGroups/ClassMappers/UserGroupMapper.cs
@@ -0,0 +1,12 @@
+﻿using DapperExtensions.Mapper;
+namespace Hl.Identity.Domain.Authorization.UserGroups.ClassMappers
+{
+    public class UserGroupMapper : ClassMapper<UserGroup>
+    {
+        public UserGroupMapper()
+        {
+            Table("auth_user_group");
+            AutoMap();
+        }
+    }
+}
---
+++ src/Servers/Identity/Hl.Identity.Domain/Authorization/UserGroups/UserGroup.cs
@@ -0,0 +1,18 @@
+﻿using Hl.Core.Enums;
+using Surging.Core.Domain.Entities.Auditing;
+
+namespace Hl.Identity.Domain.Authorization.UserGroups
+{
+    public class UserGroup : FullAuditedEntity<long>
+    {
+        public UserGroup()
+        {
+            Status = Status.Valid;
+        }
+
+        public string ParentId { get; set; }
+        public string GroupName { get; set; }
+        public Status Status { get; set; }
+
+    }
+}
